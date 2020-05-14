538efc9a31e2670c9758597b2cc84ad79eb8da33
chrpng <chrpng.brd@gmail.com>
2020-05-14 17:56:37 -0400

    Updates

---
+++ precache-manifest.4eb0979c5c6937172b314d7073f07aee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a833c598f787bba9cc3b41fc7f1cbb3",
+    "url": "/spaymart/index.html"
+  },
+  {
+    "revision": "f9d1b0513054f3d13083",
+    "url": "/spaymart/static/css/main.27eca24f.chunk.css"
+  },
+  {
+    "revision": "7996ea1002fee3498f19",
+    "url": "/spaymart/static/js/2.94c4d57b.chunk.js"
+  },
+  {
+    "revision": "bf71ac297fcfe9a3d2d199a48fc3f60c",
+    "url": "/spaymart/static/js/2.94c4d57b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9d1b0513054f3d13083",
+    "url": "/spaymart/static/js/main.368bef0b.chunk.js"
+  },
+  {
+    "revision": "6ef9d402b647b86940c5",
+    "url": "/spaymart/static/js/runtime-main.c6acd950.js"
+  },
+  {
+    "revision": "50885013ef27c11aee260f8f5ab7825f",
+    "url": "/spaymart/static/media/hero.50885013.png"
+  }
+]);
